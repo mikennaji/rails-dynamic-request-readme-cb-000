a79321ca3d77db3aaffa7c6e4a1058aaa6354fcb
Michael Nnaji <mnnaji3@uwo.ca>
2021-04-24 21:35:40 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,7 +12,7 @@ describe 'navigate' do
 
   it 'shows the description on the show page in a p tag' do
     visit "/posts/#{@post.id}"
-    expect (page).to have_css("p", text: "My post desc")
+    expect(page).to have_css("p", text: "My post desc")
   end
 
 end 
